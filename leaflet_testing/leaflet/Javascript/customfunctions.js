6c3565d879abbc33867e1bfe10b16c9f85eeefa3
Gavin <65138508+gavleavitt@users.noreply.github.com>
2020-05-12 17:49:24 -0700

    Bug fix and feature additions
    Fixed issue with shell file not starting scripts and servers
    Added logic to display text based on location

--- leaflet_testing/leaflet/Javascript/customfunctions.js
+++ leaflet_testing/leaflet/Javascript/customfunctions.js
@@ -6,14 +6,17 @@ function format_time(jsontime){
 
 
 function timedif(jsontime){
-  /*see https://stackoverflow.com/questions/1322732/convert-seconds-to-hh-mm-ss-with-javascript */
+  /**
+
+  see https://stackoverflow.com/questions/1322732/convert-seconds-to-hh-mm-ss-with-javascript */
   currenttime = new Date();
   datetime = new Date(jsontime);
   msdif = (currenttime - datetime);
   seconds = msdif/1000;
   hours = seconds/3600;
   if (hours > 24){
-    return "Over a day old!"
+    res = "Over a day old!"
+    return res;
   }else if (24 > hours > 1){
     //Hours, minutes
     dif = new Date(msdif).toISOString().substr(11, 5);
@@ -24,11 +27,53 @@ function timedif(jsontime){
     dif = new Date(msdif).toISOString().substr(14, 5);
     form_dif = dif.substr(0,2) + " Minutes " + dif.substr(3,2) + " Seconds ago";
     return form_dif;
-  } else if(seconds < 60){
+  }else if(seconds < 60){
     return "Less than a minute ago!";
-    //dif = new Date(msdif).toISOString().substr(17, 2)
-    //form_dif = dif + " Seconds ago"
   }else{
     return "Time difference error!";
   }
+}
+
+function locationtext(poi,city,county,trail,disttrail,road,distroad){
+  /**
+  Formats location responses
+   */
+   if (["Home","Work"].includes(poi)){
+     location_text =  "Location:<br><b>" + poi + "</b>";
+     display_text = region(city,county);
+   }else if (poi !== null){
+     trail_text = trailinfo(poi,trail,disttrail);
+     region_text = region(city,county);
+     display_text = "Location:" + "<br><b>" + poi + "</b>" + trail_text + region_text;
+   }else{
+     display_text = region(city,county);
+   }
+  road_text = "<br> Nearest road:<br><b>" + road + " (" +
+  distroad.toString().split(".")[0] + " feet away)</b>"
+  display_text += road_text
+  return display_text
+  }
+
+
+function region(city,county){
+  /**
+
+  */
+  if (city !== null){
+    cityloc = "<br>City:<br><b>" + city + "</b>";
+  }else{
+    cityloc = "<br>City:<br><b>Not in a CA city</b>";
+  }
+  if (county !== null){
+    countyloc = "<br>County:<br><b>" + county + "</b>";
+  }else{
+    countyloc = "<br>County:<br><b>Not in California!</b>";
+  }
+  res = cityloc + countyloc;
+  return res;
+}
+
+function trailinfo(poi,trail,disttrail){
+  res = "<br>Nearest trail is:<br><b>" + trail + "<br>(" + disttrail.toString().split(".")[0] + " feet away</b>)";
+  return res;
 }
