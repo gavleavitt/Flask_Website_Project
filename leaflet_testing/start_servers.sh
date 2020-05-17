4bab7ad2ad648326c29dba508a3d8dbb2aa2e53a
Gavin <65138508+gavleavitt@users.noreply.github.com>
2020-05-17 15:32:27 -0700

    gitignore changes

--- leaflet_testing/start_servers.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-lxterminal -e python3 /media/sf_OSGeoLive_Shared/MTB_Tracking/leaflet_testing/server.py &
-lxterminal -e python3 /media/sf_OSGeoLive_Shared/MTB_Tracking/leaflet_testing/python/GoogleDriveGPSListening.py &
-lxterminal -e geoserver_start.sh &
-/bin/bash
