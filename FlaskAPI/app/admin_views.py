68ef3e75db7d7806206fe80556a0d175e489346a
Gavin <65138508+gavleavitt@users.noreply.github.com>
2020-05-19 20:39:29 -0700

    Brought project into flask

---
+++ FlaskAPI/app/admin_views.py
@@ -0,0 +1,5 @@
+from app import app
+
+@app.route("/admin/dashboard")
+def admin_dashboard():
+    return "Admin dashboard"
---
+++ FlaskAPI/app/views.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template
+
+
+@app.route("/")
+def index():
+    return "Hello world"
+
+@app.route("/about")
+def about():
+    return "All about Flask"
+
+@app.route("/gps")
+def gps():
+    return render_template("private/tracker.html")
+
+@app.route("/main")
+def main():
+    return render_template("public/index.html")
