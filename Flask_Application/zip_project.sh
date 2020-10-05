4c392ef21b2041b1810ae4eb82622a125b4f4b26
Gavin <65138508+gavleavitt@users.noreply.github.com>
2020-10-05 14:02:36 -0700

    edited zip project to ignore windows deployment bat file

--- Flask_Application/zip_project.sh
+++ Flask_Application/zip_project.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip -r flaskdeployment.zip . -x ".flaskenv" ".env"  __pycache__/\* \*.zip migrations/\* application/__pycache__/\* *zip*
+zip -r flaskdeployment.zip . -x ".flaskenv" "zip_project_windows.bat" ".env"  __pycache__/\* \*.zip migrations/\* application/__pycache__/\* *zip*
