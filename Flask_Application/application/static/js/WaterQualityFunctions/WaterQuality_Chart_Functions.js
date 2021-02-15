e226cd60bdcfa8c28e2f86682b936b9f7ba2559b
Gavin <65138508+gavleavitt@users.noreply.github.com>
2021-02-15 15:19:33 -0800

    Further work on report history
    - Tabbing between current and history appears to work properly
    - Popup re-sizes between history and current to give more room for the history graph
    - History chart icon type and color change based on beach status

--- Flask_Application/application/static/js/WaterQualityFunctions/WaterQuality_Chart_Functions.js
+++ Flask_Application/application/static/js/WaterQualityFunctions/WaterQuality_Chart_Functions.js
@@ -1,54 +1,97 @@
 function createChart(beachName){
+  console.log("Clicked history button!")
   // Query API for records
   getBeachHistory(beachName).then(function(beachHistory){
-    console.log("Creating beach history chart")
+    // console.log("Creating beach history chart")
+
+    // Set chart display div to display block
+    var historyChartContent = document.getElementById('hist-Tab');
+    historyChartContent.style.display = "block";
     var historyChartDiv = document.getElementById('history-chart').getContext('2d');
     // Destroy chart if already existing
     if(window.historyChart instanceof Chart)
       {
-        console.log("Destroying exsiting chart!")
+        // console.log("Destroying exsiting chart!")
         window.historyChart.destroy();
       }
+    resizePopupforChart()
     var parsedData = parseBeachData(beachHistory);
     // https://stackoverflow.com/questions/43090102/chartjs-mapping-non-numeric-y-and-x
-    console.log("Creating new chart!")
+    // console.log("Creating new chart!")
     historyChart = new Chart(historyChartDiv, {
       type: 'line',
-      options: {
-        // responsive: true,
-        scales: {
-          // xAxes: [{
-          //   type: 'time',
-          //   time :{
-          //     unit: "day"
-          //   }
-          //   // position: 'left',
-          //   // ticks: {
-          //   //   labels: ["Open","Warning", "Closed"]
-          //   // }
-          // }],
-          yAxes:[{
-            type:'category',
-            position: "left"
-          }]
-          // yAxes:[{
-          //   type: 'category',
-          //   ticks:{
-          //     labels: ["Open","Warning", "Closed"]
-          //   }
-          // }]
-        }
-      },
       data:{
         yLabels: ["Open","Warning","Closed"],
         xLabels: parsedData["dataX"],
         datasets:[{
+          data:parsedData["dataY"],
           showLine: false,
-          label: "Status",
-          // backgroundColor: 'rgb(255, 99, 132)',
-          // borderColor: 'rgb(255, 99, 132)',
-          data:parsedData["dataY"]
+          label: "Status"
         }]
+      },
+      options: {
+        layout:{
+          padding:{
+            right: 15
+          }
+        },
+        title: {
+          display: true,
+          text: beachName,
+          padding: 15,
+          fontSize: 15
+        },
+        legend: {
+          display: false
+        },
+        responsive: true,
+        scales: {
+          xAxes: [{
+            ticks:{
+              fontStyle: 'bold',
+              padding: 10
+            }
+          }],
+          yAxes:[{
+            type:'category',
+            position: "left",
+            ticks:{
+              labels: ["Open","Warning", "Closed"],
+              padding: 10,
+              fontStyle: 'bold'
+            }
+          }]
+        },
+        elements:{
+          point:{
+            radius: 10,
+            hoverRadius: 15,
+            pointStyle: function(context){
+              var index = context.dataIndex;
+              var value = context.dataset.data[index];
+              if (value == "Open"){
+                return 'circle'
+              }
+              else if (value == 'Warning'){
+                return "triangle"
+              } else if (value == 'Closed'){
+                return 'cross'
+              }
+            },
+            backgroundColor: function(context){
+              var index = context.dataIndex;
+              var value = context.dataset.data[index];
+              if (value == "Open"){
+                return 'rgba(81, 75, 191)'
+              }
+              else if (value == 'Warning'){
+                return 'rgba(255, 215, 100)'
+              } else if (value == 'Closed'){
+                return 'rgba(244, 67, 54)'
+              }
+            }
+          }
+        }
       },
     })
   })
@@ -68,16 +111,27 @@ function getBeachHistory(beachName){
 function parseBeachData(beachHistory){
   dataY = []
   dataX = []
-  console.log(beachHistory)
+  // console.log(beachHistory)
   for (i of Object.keys(beachHistory)){
     // console.log(beachHistory[i])
     // dataY.push(moment(beachHistory[i].date, 'DD-MM-YY').toDate());
-    dataX.push(moment(beachHistory[i].date, 'YYYY-MM-DD').format("DD-MM-YY"));
+    dataX.push(moment(beachHistory[i].date, 'YYYY-MM-DD').format("MMM-DD-YY"));
     dataY.push(beachHistory[i].status)
   }
-  console.log("Data X:")
-  console.log(dataX)
-  console.log("Data Y:")
-  console.log(dataY)
+  // console.log("Data X:")
+  // console.log(dataX)
+  // console.log("Data Y:")
+  // console.log(dataY)
   return {"dataX":dataX, "dataY":dataY}
 };
+
+function resizePopupforChart(){
+  var popupContent = document.getElementsByClassName('leaflet-popup-content-wrapper')
+  var historyContent = document.getElementById('hist-content')
+  popupContent[0].style.width = "500px";
+  historyContent.style.width = "400px";
+  popupContent[0].style.height = "300px";
+  historyContent.style.height = "200px";
+  var textContent = document.getElementById('text-content');
+  textContent.style.display = "none";
+}
