9a16aabc2ba41b24542120a1f89692de4d4f6118
Gavin <65138508+gavleavitt@users.noreply.github.com>
2021-08-12 22:32:51 -0700

    Restructured Trace SQL query
    New SQL query is working, but the renderer doesn't appear to be working properly

--- Flask_Application/application/WebAppProjects/LACO_SW_TraceApp/API_Routes.py
+++ Flask_Application/application/WebAppProjects/LACO_SW_TraceApp/API_Routes.py
@@ -26,58 +26,62 @@ def handletracerequest():
         directionSQL = downstreamSQL
     else:
         return Response(status=400)
-    # Raw sql statment, used since PG_Routing doesnt have SQLAlchemy ORM support
+    # Raw sql statement, used since PG_Routing doesnt have SQLAlchemy ORM support
     sql = ("""
-    -- Snap input coordinates to nearest network node
-with sc AS (
-	SELECT
-		node.id as id, node.the_geom as geom
-	from 
-		storm_network_vertices_pgr as node
-	where
-		st_intersects(ST_Snap(ST_Transform(ST_SetSRID(ST_Point(:lon, :lat), 4326),2229),node.the_geom, 100), node.the_geom)
-	ORDER BY 
-		node.the_geom <-> ST_Transform(ST_SetSRID(ST_Point(:lon, :lat), 4326),2229)
-	LIMIT 1
-)
-SELECT sc.id,  ST_AsGeoJSON(st_transform(sc.geom, 4326)) as startgeom, 
-i.uuid as inlet_uuid, ST_AsGeoJSON(st_transform(i.geom, 4326)) as inletgeom, 
-mh.uuid as mh_uuid, ST_AsGeoJSON(st_transform(mh.geom, 4326)) as mhgeom,
-ol.uuid as outlet_uuid, ST_AsGeoJSON(st_transform(ol.geom, 4326)) as outletgeom,
-lat.uuid as lat_uuid, ST_AsGeoJSON(st_transform(lat.geom, 4326)) as latgeom, 
-gm.uuid as gm_uuid, ST_AsGeoJSON(st_transform(gm.geom, 4326)) as gmgeom,
-fm.uuid as fm_uuid, ST_AsGeoJSON(st_transform(fm.geom, 4326)) as fmgeom,
-nodes.* FROM sc, pgr_drivingDistance(
-        :directionSQL,
-        sc.id, 999999, true) AS nodes
-LEFT join 
-	storm_network as sn
-ON 
-	(nodes.edge = sn.id)
-LEFT join
-	inlets i
-ON 
-	(nodes.node = i.node_fk)
-LEFT join
-	maintenanceholes mh
-ON 
-	(nodes.node = mh.node_fk)
-LEFT join
-	outlets ol
-ON 
-	(nodes.node = ol.node_fk)
-LEFT join
-	laterals lat 
-ON 
-	(sn.uuid = lat.uuid)
-LEFT join
-	gravitymainssplit gm
-on
-	(sn.uuid = gm.uuid)
-LEFT join
-	forced_mains fm
-on
-	(sn.uuid = fm.uuid)
+SELECT 
+	node.id as id, node.the_geom as geom, GeometryType(node.the_geom) as geomtype
+INTO TEMP TABLE sp
+FROM
+	storm_network_vertices_pgr as node
+WHERE
+	st_intersects(ST_Snap(ST_SetSRID( ST_Point(6466816.860,1847607.920), 2229),node.the_geom, 100), node.the_geom)
+ORDER BY 
+	node.the_geom <-> ST_SetSRID( ST_Point(6466816.860,1847607.920), 2229)
+LIMIT 1;
+
+SELECT sp.id, nodes.*  INTO TEMP TABLE traceresults from sp, pgr_drivingDistance(
+        'SELECT id, source, target, -1 as cost, reverse_cost FROM storm_network',
+        sp.id, 999999, true) AS nodes;
+		
+SELECT
+	mh.uuid, st_asgeojson(st_transform(mh.geom, 4326)) as geojson, mh.factype as factype, tr.cost as cost, GeometryType(mh.geom) as geomtype
+FROM 
+	maintenanceholes mh, traceresults as tr
+WHERE
+	(mh.node_fk = tr.node)
+UNION
+select 
+	i.uuid, st_asgeojson(st_transform(i.geom, 4326)) as geojson, i.factype as factype, tr.cost as cost, GeometryType(i.geom) as geomtype
+FROM 
+	inlets i, traceresults as tr
+WHERE
+	(i.node_fk = tr.node)
+UNION
+select 
+	ol.uuid, st_asgeojson(st_transform(ol.geom, 4326)) as geojson, ol.factype as factype, tr.cost as cost, GeometryType(ol.geom) as geomtype 
+FROM 
+	outlets ol, traceresults as tr
+WHERE
+	(ol.node_fk = tr.node)
+UNION
+SELECT
+	gm.uuid, st_asgeojson(st_transform(gm.geom, 4326)) as geojson, gm.factype as factype,  tr.cost as cost, GeometryType(gm.geom) as geomtype
+FROM 
+	gravitymainssplit gm, traceresults as tr
+WHERE
+	(gm.edge_fk = tr.edge)
+UNION
+SELECT
+	l.uuid, st_asgeojson(st_transform(l.geom, 4326)) as geojson, l.factype as factype,  tr.cost as cost, GeometryType(l.geom) as geomtype  
+FROM 
+	laterals l, traceresults as tr
+WHERE
+	(l.edge_fk = tr.edge)
+UNION
+SELECT
+	NULL as uuid, st_asgeojson(st_transform(sp.geom, 4326)) as geojson, 'startpoint' as factype,  '0' as cost, GeometryType(sp.geom) as geomtype
+FROM
+	sp
     """)
 
     # Lists to hold point and line results
@@ -87,68 +91,19 @@ on
     results = db.session.execute(sql, {"lat": lat, "lon": lon, "directionSQL":directionSQL})
     startpoint = None
     for i in results:
-        application.logger.debug(i)
-        # Dictionary to hold data for each loop
         propDict = {}
-        application.logger.debug(propDict)
-        if i.cost == 0.0:
-            # The snapped point will have a cost of 0 and all geoms will be null besides startgeom
-            # TODO: verify if this is correct, a point with a cost of 0.0 is also the first node
-            propDict['factype'] = "start"
-            geom = i.startgeom
-            geojsonGeom = geojson.loads(geom)
+        propDict['factype'] = i.factype
+        propDict['id'] = i.uuid
+        # propDict['factype'] = "g1234"
+        propDict['cost'] = i.cost
+        geojsonGeom = geojson.loads(i.geojson)
+        if i.factype == "startpoint":
             startpoint = Feature(geometry=Point(geojsonGeom), properties=propDict)
-            # break
-        if i.inletgeom or i.mhgeom or i.outletgeom:
-            application.logger.debug("Non null point geom!")
-            # Record is a point feature, populate point features list depending on which geometry is not null
-            if i.inletgeom:
-                application.logger.debug("Inlet!")
-                propDict['id'] = i.inlet_uuid
-                propDict['factype'] = "inlet"
-                geom = i.inletgeom
-            elif i.mhgeom:
-                application.logger.debug("MH!")
-                propDict['id'] = i.mh_uuid
-                propDict['factype'] = "manhole"
-                geom = i.mhgeom
-            elif i.outletgeom:
-                application.logger.debug("Outlet!")
-                propDict['id'] = i.outlet_uuid
-                propDict['factype'] = "outlet"
-                geom = i.outletgeom
-            else:
-                propDict = None
-                geom = None
-            # Set the cost property, not currently used
-            propDict['cost'] = i.cost
-            # Load st_asgeojson result as a geojson object
-            geojsonGeom = geojson.loads(geom)
-            # Create a point feature using the geometry and properties, append to point features list
-            application.logger.debug(propDict)
+        elif i.geomtype == "POINT":
             pointfeatures.append(Feature(geometry=Point(geojsonGeom), properties=propDict))
-        # Populate line features, only one should always be populated
-        # A single record can have both a line and a point geom so these are not in a else statement with the prior
-        # if statement.
-        if i.gmgeom:
-            propDict['id'] = i.gm_uuid
-            propDict['factype'] = "gravitymain"
-            geom = i.gmgeom
-        elif i.latgeom:
-            propDict['id'] = i.lat_uuid
-            propDict['factype'] = "lateral"
-            geom = i.latgeom
-        elif i.fmgeom:
-            propDict['id'] = i.fm_uuid
-            propDict['factype'] = "forcedmain"
-            geom = i.fmgeom
-        else:
-            break
-            propDict = None
-            geom = None
-        geojsonGeom = geojson.loads(geom)
-        linefeatures.append(Feature(geometry=MultiLineString(geojsonGeom), properties=propDict))
-    # Format json response, will have nested goejson data
+        elif i.geomtype == "LINESTRING":
+            linefeatures.append(Feature(geometry=MultiLineString(geojsonGeom), properties=propDict))
+    # Format json response, will have nested geojson data
     lineCollection = FeatureCollection(linefeatures)
     pointsCollection = FeatureCollection(pointfeatures)
     response = jsonify({"startpoint": startpoint, "lines": lineCollection, "points": pointsCollection})
--- Flask_Application/application/WebAppProjects/LACO_SW_TraceApp/static/SBCO_SW_TraceApp/js/renderers.js
+++ Flask_Application/application/WebAppProjects/LACO_SW_TraceApp/static/SBCO_SW_TraceApp/js/renderers.js
@@ -4,30 +4,78 @@ const pointResultRenderer = {
     title: "Structures"
   },
   field: "factype",
+  defaultSymbol: { type: "simple-marker" },
   uniqueValueInfos: [{
-    value: "S",
-    label: "State highway",
+    value: "Inlet",
+    label: "Inlet",
+    symbol: {
+      type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+      size: 4,
+      style: "square",
+      color: "blue"
+      // outline: {  // autocasts as new SimpleLineSymbol()
+      //   width: 1,
+      //   color: "white"
+      // }
+    }
+  }, {
+    value: "Maintenance Holes",
+    label: "Maintenance Holes",
+    symbol: {
+      type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+      size: 4,
+      color: "red",
+      outline: {  // autocasts as new SimpleLineSymbol()
+        width: 1,
+        color: "white"
+      }
+    }
+    // symbol: getUniqueValueSymbol("/static/icons_images/manhole_yellow.svg")
+  }, {
+    value: "Outlets",
+    label: "Outlet",
+    symbol: {
+      type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+      size: 4,
+      color: "red",
+      outline: {  // autocasts as new SimpleLineSymbol()
+        width: 1,
+        color: "white"
+      }
+    }
+  }]
+};
+
+const lineResultRenderer = {
+  type: "unique-value",
+  legendOptions: {
+    title: "Drainage"
+  },
+  field: "factype",
+  uniqueValueInfos: [{
+    value: "Laterals",
+    label: "Laterals",
     symbol: {
       type: "simple-line",
-      color: "#e6d800",
+      color: "#1bcc4a",
       width: "6px",
       style: "solid"
     }
   }, {
-    value: "I",
-    label: "Interstate",
+    value: "Gravity Mains",
+    label: "Gravity Mains",
     symbol: {
       type: "simple-line",
-      color: "#e60049",
+      color: "#0f1cd4",
       width: "6px",
       style: "solid"
     }
-  }, {
-    value: "U",
-    label: "US Highway",
+  },{
+    value: "Force Mains",
+    label: "Force Mains",
     symbol: {
       type: "simple-line",
-      color: "#9b19f5",
+      color: "#d40f0f",
       width: "6px",
       style: "solid"
     }
