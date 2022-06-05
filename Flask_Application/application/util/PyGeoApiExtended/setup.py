709f994dfbd0a2720b5ca732461790c04a4cd04c
gavleavitt <65138508+gavleavitt@users.noreply.github.com>
2022-06-05 11:00:21 -0700

    Fixed pygeoapi custom providers package setup

--- Flask_Application/application/util/PyGeoApiExtended/setup.py
+++ Flask_Application/application/util/PyGeoApiExtended/setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 # see https://betterscientificsoftware.github.io/python-for-hpc/tutorials/python-pypi-packaging/
 setup(
     name='pygeoapicustom',
     version='0.1.0',
-    packages=['extendedproviders'],
+#    packages=['extendedproviders'],
+    packages=find_packages(),
     install_requires=['mpi4py>=2.0',
                       'numpy',
                       ],
